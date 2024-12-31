2084fb9957fb3379e93698f6d18392c5c1e564d3
Lalit1223 <128039870+Lalit1223@users.noreply.github.com>
2024-12-31 18:04:01 +0530

    final

--- routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login } = require("../controllers/adminController");
-
-// Login route
-router.post("/login", login);
-
-module.exports = router;
